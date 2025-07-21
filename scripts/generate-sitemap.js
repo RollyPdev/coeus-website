68d5d48dce0ba8751e8ba5c4c2d89adcdb516b87
Rolly Paredes <coeusrolly@gmail.com>
2025-07-21 22:18:55 +0800

    Initial commit

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -17,16 +17,11 @@ const routes = [
 // Get current date in YYYY-MM-DD format
 const currentDate = new Date().toISOString().split('T')[0];
 
+// Add favicon.ico to the sitemap
+const faviconEntry = `  <url>\n    <loc>${BASE_URL}/favicon.ico</loc>\n    <lastmod>${currentDate}</lastmod>\n    <changefreq>yearly</changefreq>\n    <priority>0.1</priority>\n  </url>`;
+
 // Generate sitemap XML
-const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${routes.map(route => `  <url>
-    <loc>${BASE_URL}${route.url}</loc>
-    <lastmod>${currentDate}</lastmod>
-    <changefreq>${route.changefreq}</changefreq>
-    <priority>${route.priority}</priority>
-  </url>`).join('\n')}
-</urlset>`;
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${routes.map(route => `  <url>\n    <loc>${BASE_URL}${route.url}</loc>\n    <lastmod>${currentDate}</lastmod>\n    <changefreq>${route.changefreq}</changefreq>\n    <priority>${route.priority}</priority>\n  </url>`).join('\n')}\n${faviconEntry}\n</urlset>`;
 
 // Write sitemap to public directory
 fs.writeFileSync(
