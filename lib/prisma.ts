0aca93fea3b51d18b1dec6b82a6ac1f6131f5e98
Rollypdev <coeusrolly@gmail.com>
2025-08-30 21:54:25 +0800

    ss

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-const prisma = globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query', 'error', 'warn'],
-  })
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 export default prisma
